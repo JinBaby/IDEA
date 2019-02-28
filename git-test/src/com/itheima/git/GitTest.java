2c48036136944272b477614f369deff5f39feb52
JIN <jinyize1997@qq.com>
2019-02-28 21:06:37 +0800

    dev2 再次修改

--- git-test/src/com/itheima/git/GitTest.java
+++ git-test/src/com/itheima/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
 
-        System.out.println("git git git");
+        System.out.println("dev2 git git git");
     }
 }
